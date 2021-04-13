5162529497c9e58c2dade8a26163351feda0fe9b
maja3494 <maja3494@colorado.edu>
2021-04-13 16:59:33 -0600

    basic code to overlay SWE and water level data

--- example_water_output.py
+++ example_water_output.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
-import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+import read_all_sno_water_eq_data from snotel_data_retriever
 
 def ReadWaterFile(filepath):
     file=open(filepath,'r')
@@ -14,27 +15,48 @@ def ReadWaterFile(filepath):
             year=split[7]
             day=split[6]
             month=split[5]
-            # print(year+'-'+month+'-0'+day+)
-            date=month+'/'+day+'/'+year
-            x.append(date)
-            y.append(split[-1][:-1])
-    dates = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in x]
-    return dates, y
+            date=year+'-'+month.zfill(2)+'-'+day.zfill(2)
+            x.append(np.datetime64(date))
+            y.append(int(split[-1][:-1]))
+    return x, y
 
 
 
 def plot1year(filepath):
     dates, water_vals=ReadWaterFile(filepath)
-    for i, station in enumerate(sntl_stations_data):
-            times = np.asarray(pd.to_datetime(np.linspace(pd.Timestamp(station.beginDate).value,
-                                                          pd.Timestamp(station.endDate).value, len(station.values))))
-            ax.plot_date(times, station.values, '-', label=f"{station.stationTriplet} ({sntl_stations_metadata[i].name})")
+    fig, ax = plt.subplots(1, 1)
+    ax.plot_date(dates, water_vals, '-', label=f"{filepath}")
+    ax.set_title(f"Water level data in {filepath}")
+    ax.set_ylabel("Water Level (cfs)")
+    ax.set_xlabel("Dates")
+    ax.xaxis.set_major_formatter(DateFormatter('%m/%d/%y'))
+    # ax.set_yticks(range(0,max(water_vals,10))
+    plt.show()
 
-        ax.set_title(f"Snow water Eq in {county_name} County ({sntl_stations_data[0].beginDate[:10]} to "
-                     f"{sntl_stations_data[0].endDate[:10]})")
-        ax.set_ylabel("Snow water Eq (in)")
-        ax.set_xlabel("Time")
-        ax.xaxis.set_major_formatter(DateFormatter('%m/%d/%y')
-    plt.savefig('water_level.png')
+def OverlayData():
+    dates, water_vals=ReadWaterFile(filepath)
 
-plot1year("./WaterData/06700000/06700000_2007.csv")
+    fig, ax = plt.subplots(1, 1)
+
+    
+    ax.plot_date(dates, water_vals, '-', label=f"{filepath}")
+    ax.plot_date(dates, water_vals, '-', label=f"{filepath}")
+    ax.set_title(f"Water level data in {filepath}")
+    ax.set_ylabel("Water Level (cfs)")
+    ax.set_xlabel("Dates")
+    ax.xaxis.set_major_formatter(DateFormatter('%m/%d/%y'))
+
+    '''plot 1^^^'''
+    '''plot 2vvv'''
+    ax2=ax.twinx()
+    all_data = read_all_sno_water_eq_data('dataset', 'Boulder Creek', ['838', '663'])
+    station_name, dataset_data = all_data[0]
+    file, data = dataset_data[-3]
+    ax2.plot_date(data['date'], data['value'], '-', label=f"Snow Water Eq Data")
+    ax2.set_ylabel("Snow water Eq (in)")
+
+
+    plt.show()
+
+plot1year("./WaterData/06700000/06700000_2019.csv")
+OverlayData("./WaterData/06700000/06700000_2019.csv")
